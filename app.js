56409a0e769e99ac55521e853dd7281b4773ae28
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-10 14:26:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+buttonEl.addEventListener('click', (e) => {
+    const enteredValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEl.appendChild(listItemEl);
+    inputEl.value = '';
+})
