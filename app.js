15d6a503687d476fe50dc5917db37cc4b5f3a3ee
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-10 14:51:01 +0200

    Migrate app to Vue framework

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
+
+/*
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
@@ -8,4 +24,4 @@ buttonEl.addEventListener('click', (e) => {
     listItemEl.textContent = enteredValue;
     listEl.appendChild(listItemEl);
     inputEl.value = '';
-})
+})*/
